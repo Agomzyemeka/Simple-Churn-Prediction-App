99eac481ad9007066b785ec2f6028ed130917404
AGOMOH CHUKWUEMEKA <113604626+Agomzyemeka@users.noreply.github.com>
2024-06-28 20:02:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ except Exception as e:
 
 # Streamlit UI
 st.title('Customer Churn Prediction')
+st.write("Churn is the measure of how many customers stop using a product. This can be measured based on actual usage or failure to renew (when the product is sold using a subscription model).")
 
 st.header('Enter customer details:')
 
